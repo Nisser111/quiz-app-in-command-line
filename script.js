2ee34bcb3175047c4bfa5de9738675040d37d9a9
Nisser111 <kulakmilosz2006@gmail.com>
2023-08-28 22:49:51 +0200

    Transform functions to asynchronic. Function getAnswer is ready.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const commandLine = document.querySelector("main");
-
-let questions = [];
-
 class Question {
   constructor(question, answers, correctAns) {
     let [_ansA, _ansB, _ansC, _ansD] = [...answers];
@@ -52,189 +48,230 @@ class Question {
   }
 }
 
-let showCommonLine = (h, c, isError = false) => {
-  if ("content" in document.createElement("template")) {
-    const template = document.querySelector("#commonLine");
-    const clone = template.content.cloneNode(true);
+function showCommandLine() {}
 
-    const header = clone.querySelector(".header");
-    const content = clone.querySelector(".line");
+function showError(lineHeader, content) {
+  // Create element
+  const commandLine = document.querySelector("main");
 
-    header.innerText += h;
-    content.innerHTML += c;
+  let line = document.createElement("div");
+  line.classList.add("line");
+  line.style.color = "var(--errorFontColor)";
 
-    if (isError) content.style.color = "var(--errorFontColor)";
+  let span = document.createElement("span");
+  span.innerText = "programmer-quiz\\";
 
-    commandLine.append(clone);
-  }
-};
+  let header = document.createElement("span");
+  header.classList.add("header");
+  header.innerText = lineHeader;
 
-function getAnswer(h, trueValues) {
-  if ("content" in document.createElement("template")) {
-    const template = document.querySelector("#answerLine");
-    const clone = template.content.cloneNode(true);
+  span.appendChild(header);
+  span.innerHTML += "> ";
 
-    const header = clone.querySelector(".header");
-    header.innerText += h;
+  line.appendChild(span);
 
-    commandLine.append(clone);
-  }
+  line.innerText += content;
 
-  const answerFields = document.querySelectorAll(".answer");
-  let lastAnswerField = answerFields[answerFields.length - 1];
-  lastAnswerField.innerHTML = "";
-  lastAnswerField.focus();
+  commandLine.appendChild(line);
+}
 
-  lastAnswerField.addEventListener("keydown", (e) => {
-    if (e.keyCode == 13) {
-      let value = lastAnswerField.innerHTML;
-      value = lastAnswerField.innerHTML;
-      e.target.contentEditable = "false";
+function getAnswer(lineHeader) {
+  return new Promise((resolve) => {
+    // Create element
+    const commandLine = document.querySelector("main");
 
-      // if (value.toLowerCase() in trueValues) {
-      //   window.currentAnswer = value;
-      //   validFlag = true;
-      // } else {
-      //   showCommonLine("error", "Incorrect data value", true);
-      // }
+    let line = document.createElement("div");
+    line.classList.add("line");
 
-      // validation
-      
-      var validFlag = false;
-      do {
-        for (let element of trueValues) {
-          if (value.toLowerCase() == element) {
-            window.currentAnswer = value;
-            validFlag = true;
-          } else {
-            showCommonLine("error", "Incorrect data value", true);
-          }
-        }
-      } while(!validFlag);
-    }
+    let span = document.createElement("span");
+    span.innerText = "programmer-quiz\\";
+
+    let header = document.createElement("span");
+    header.classList.add("header");
+    header.innerText = lineHeader;
+
+    span.appendChild(header);
+    span.innerHTML += "> ";
+
+    line.appendChild(span);
+
+    let answerField = document.createElement("div");
+    answerField.classList.add("answer");
+    answerField.setAttribute("contenteditable", "true");
+    answerField.innerHTML = "";
+
+    line.appendChild(answerField);
+
+    commandLine.appendChild(line);
+
+    // Focus div
+    const answerFields = document.querySelectorAll(".answer");
+    let lastAnswerField = answerFields[answerFields.length - 1];
+
+    lastAnswerField.innerHTML = null;
+    lastAnswerField.focus();
+
+    // Submit answer
+    lastAnswerField.addEventListener("keydown", (e) => {
+      if (e.keyCode == 13) {
+        e.target.setAttribute("contenteditable", "false");
+        resolve(e.target.innerText.toLowerCase().trim());
+      }
+    });
   });
 }
 
-// adding questions
-questions.push(
-  new Question(
-    "Which of the following languages is not used for frontend development?",
-    ["HTML", "Java", "CSS", "JavaScript"],
-    "Java"
-  )
-);
+function validateAnswer(value, avaibleAsnwers) {
+  return new Promise((resolve, reject) => {
+    if (Array.isArray(avaibleAsnwers)) resolve(avaibleAsnwers.includes(value));
+    else
+      reject(
+        "Sorry! We have some problems. We will fix them as soon as possible."
+      );
+  });
+}
 
-questions.push(
-  new Question(
-    "What does CSS stand for?",
-    [
-      "Cascading Style Sheets",
-      "Creative Style System",
-      "Computer Style Sheets",
-      "Colorful Style Symbols",
-    ],
-    "Cascading Style Sheets"
-  )
-);
+async function getUserAnswer(lineHeader, avaibleAsnwers) {
+  let userEnteredCorrectly = false;
 
-questions.push(
-  new Question(
-    "Which HTML tag is used to define an unordered list?",
-    ["&lt;ul&gt;", "&lt;ol&gt;", "&lt;li&gt;", "&lt;list&gt;"],
-    "&lt;ul&gt;"
-  )
-);
+  while (!userEnteredCorrectly) {
+      const userAns = await getAnswer(lineHeader);
 
-questions.push(
-  new Question(
-    "What is the purpose of the JavaScript function `querySelector()`?",
-    [
-      "To select an HTML element by its class or ID",
-      "To perform mathematical calculations",
-      "To add event listeners",
-      "To change the page URL",
-    ],
-    "To select an HTML element by its class or ID"
-  )
-);
+    if (await validateAnswer(userAns, avaibleAsnwers)) {
+      userEnteredCorrectly = true;
+      return userAns;
+    } else {
+      showError(lineHeader, "We don't have that answer. Please type again...");
+    }
+  }
+}
 
-questions.push(
-  new Question(
-    "Which of the following is a valid way to declare a CSS class?",
-    [".my-class", "#my-class", "class.my-class", "&lt;my-class&gt;"],
-    ".my-class"
-  )
-);
+// if(Array.isArray(avaibleAsnwers)) {
+//   if (avaibleAsnwers.includes(value)) return value; // replace to return
+//   else {
+//     showError(lineHeader, "We don't have that answer. Please type again...");
+//     return false;
+//   };
+// } else {
+//   alert("Sorry! We have some problems. We will fix them as soon as possible.");
+// }
 
-questions.push(
-  new Question(
-    "What is the correct syntax for a JavaScript `for` loop?",
-    [
-      "for (i = 0; i < 5; i++)",
-      "for (var i = 0; i < 5; i++)",
-      "for (i < 5; i++)",
-      "for (i = 0; i++)",
-    ],
-    "for (i = 0; i < 5; i++)"
-  )
-);
+window.onload = async function () {
+  let questions = [];
+  let userScore = 0;
 
-questions.push(
-  new Question(
-    "What does the acronym API stand for in web development?",
-    [
-      "Advanced Programming Interface",
-      "Application Programming Interface",
-      "Automated Programming Interface",
-      "Associated Program Interface",
-    ],
-    "Application Programming Interface"
-  )
-);
-
-questions.push(
-  new Question(
-    "Which of the following is used to make a webpage responsive to different screen sizes?",
-    ["Media queries", "JavaScript", "CSS selectors", "HTML forms"],
-    "Media queries"
-  )
-);
-
-questions.push(
-  new Question(
-    "What is the purpose of the HTML &lt;canvas&gt; element?",
-    [
-      "To display images",
-      "To create animations and graphics",
-      "To embed videos",
-      "To style text",
-    ],
-    "To create animations and graphics"
-  )
-);
-
-questions.push(
-  new Question(
-    "What does the CSS property `display: none;` do?",
-    [
-      "Increases the font size",
-      "Changes the background color",
-      "Makes an element visible on hover",
-      "Hides an element from the webpage",
-    ],
-    "Hides an element from the webpage"
-  )
-);
-
-window.onload = function () {
-  showCommonLine(
-    "start",
-    `Hi! Do you think you're a good programmer? Let's check this. Type <span class="marked">GO</span> to start the quiz...`
+  // adding questions
+  questions.push(
+    new Question(
+      "Which of the following languages is not used for frontend development?",
+      ["HTML", "Java", "CSS", "JavaScript"],
+      "Java"
+    )
   );
 
-  getAnswer("start", ["go"]);
+  questions.push(
+    new Question(
+      "What does CSS stand for?",
+      [
+        "Cascading Style Sheets",
+        "Creative Style System",
+        "Computer Style Sheets",
+        "Colorful Style Symbols",
+      ],
+      "Cascading Style Sheets"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "Which HTML tag is used to define an unordered list?",
+      ["&lt;ul&gt;", "&lt;ol&gt;", "&lt;li&gt;", "&lt;list&gt;"],
+      "&lt;ul&gt;"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "What is the purpose of the JavaScript function `querySelector()`?",
+      [
+        "To select an HTML element by its class or ID",
+        "To perform mathematical calculations",
+        "To add event listeners",
+        "To change the page URL",
+      ],
+      "To select an HTML element by its class or ID"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "Which of the following is a valid way to declare a CSS class?",
+      [".my-class", "#my-class", "class.my-class", "&lt;my-class&gt;"],
+      ".my-class"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "What is the correct syntax for a JavaScript `for` loop?",
+      [
+        "for (i = 0; i < 5; i++)",
+        "for (var i = 0; i < 5; i++)",
+        "for (i < 5; i++)",
+        "for (i = 0; i++)",
+      ],
+      "for (i = 0; i < 5; i++)"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "What does the acronym API stand for in web development?",
+      [
+        "Advanced Programming Interface",
+        "Application Programming Interface",
+        "Automated Programming Interface",
+        "Associated Program Interface",
+      ],
+      "Application Programming Interface"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "Which of the following is used to make a webpage responsive to different screen sizes?",
+      ["Media queries", "JavaScript", "CSS selectors", "HTML forms"],
+      "Media queries"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "What is the purpose of the HTML &lt;canvas&gt; element?",
+      [
+        "To display images",
+        "To create animations and graphics",
+        "To embed videos",
+        "To style text",
+      ],
+      "To create animations and graphics"
+    )
+  );
+
+  questions.push(
+    new Question(
+      "What does the CSS property `display: none;` do?",
+      [
+        "Increases the font size",
+        "Changes the background color",
+        "Makes an element visible on hover",
+        "Hides an element from the webpage",
+      ],
+      "Hides an element from the webpage"
+    )
+  );
+
+  const appStart = await getUserAnswer("start", ["go"]);
+
 };
 
-//   for (let q in questions) {
-//     questions[q].showQuestion(q);
-//   }
